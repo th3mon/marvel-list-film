cad8048de04543d4a8a219832d7e501a52cfc539
Mateusz Szuber <mateusz@MacBook-Air-Mateusz.local>
2018-04-21 21:35:20 +0200

    test

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="matiTest">
+        <span> {{ message }} </span>
+    </div>
+</template>
+
+<script>
+export default { 
+  name: 'Button',
+  data() {
+      return {
+          message: "Pierwszy dodany komponent"
+      };
+  }
+};
+</script>
---
+++ src/components/Test2.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="name">
+      {{name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Name',
+  data () {
+      return {
+          name: 'raz, dwa, trzy',
+      };
+  }
+};
+</script>
