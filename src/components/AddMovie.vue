6428b4b430e9545fec2af2d7eb360a6b4ffa7d31
Mateusz Szuber <mateusz@MacBook-Air-Mateusz.local>
2018-05-08 19:21:09 +0200

    t

---
+++ src/components/AddMovie.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="addMovie">
+        <input v-model="characterName" type="text" placeholder="Wprowadź bohatera" />
+        <a v-on:click="addCharacter(characterName, $event)" href="#">
+            +
+        </a>
+    </div>
+</template>
+
+<script>
+export default { 
+  name: 'AddMovie',
+  data () {
+      return {
+          characterName: '',
+      };
+  },
+  methods: {
+      addCharacter (value, event) {
+          event.preventDefault();
+      }
+  }
+};
+</script>
