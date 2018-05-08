ba56909303cd9b5d5fe82ff7754e339d756ae834
Mateusz Szuber <mateusz@MacBook-Air-Mateusz.local>
2018-05-08 20:53:13 +0200

    v-for, data, methods

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -1,24 +1,40 @@
+
 <template>
     <div class="addMovie">
         <input v-model="characterName" type="text" placeholder="Wprowadź bohatera" />
         <a v-on:click="addCharacter(characterName, $event)" href="#">
             +
         </a>
+        <ul>
+            <li 
+                v-for="singleItem in listItems"
+                v-bind:key="singleItem.listItem"
+            >
+            <a href="#">
+                {{singleItem.listItem}}
+            </a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default { 
-  name: 'AddMovie',
-  data () {
-      return {
-          characterName: '',
-      };
+export default {
+  name: "AddMovie",
+  data() {
+    return {
+      characterName: "",
+      listItems: [
+
+      ],
+    };
   },
   methods: {
-      addCharacter (value, event) {
-          event.preventDefault();
-      }
+    addCharacter(value, event) {
+        event.preventDefault();
+        let inputValue = value;
+        let actualList = [this.$data.listItems];
+    }  
   }
 };
 </script>
